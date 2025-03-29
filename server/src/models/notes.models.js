4168fc193d3b3a086c097b811ce64ec81da6acc5
Sowdarjya <kolaysowdarjya@gmail.com>
2025-03-29 16:25:27 +0530

    feat: add note model and integrate react-dropzone for file uploads

---
+++ server/src/models/notes.models.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const noteSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    fileUrl: {
+      type: String,
+      required: true,
+    },
+    fileType: {
+      type: String,
+      enum: ["image", "pdf"],
+      required: true,
+    },
+    uploadedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  { timestamps: true }
+);
+
+const Note = mongoose.model("Note", noteSchema);
