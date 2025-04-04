f1b9e0f221d23238feb772a560fe55259ba5f7a3
Sowdarjya Kolay <161492002+Sowdarjya@users.noreply.github.com>
2025-04-05 01:25:00 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: "https://note-org-binary.vercel.app/",
+    origin: "https://note-org-binary.vercel.app",
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
